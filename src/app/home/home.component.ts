aa9671dc1bcc6f4bd2e01a9bfb914012c0433f42
Danilo Pereira <danilo-gc@hotmail.com>
2019-01-08 19:08:59 -0300

    promisses e service

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -7,17 +7,25 @@ import { Oferta } from '../shared/oferta.model';
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css'],
-  providers: [ OfertasService ]
+  providers: [OfertasService]
 })
 export class HomeComponent implements OnInit {
-
   public ofertas: Oferta[];
 
   constructor(private ofertasService: OfertasService) {}
 
   ngOnInit() {
-      this.ofertas = this.ofertasService.getOfertas();
-      console.log(this.ofertas);
-  }
+    // this.ofertas = this.ofertasService.getOfertas();
+    // console.log(this.ofertas);
 
+    this.ofertasService
+      .getOfertas2()
+      .then((ofertas: Oferta[]) => {
+        console.log('a função foi resolvida depois de 3s.');
+        this.ofertas = ofertas;
+      })
+      .catch((param: any) => {
+        console.log(param);
+      });
+  }
 }
